f78ebf967e811fb4e19a208368a6e50d4cbaf291
jabca <pavel.sterlin.work@gnail.com>
2022-12-10 11:56:06 +0300

    Create db schema

--- api/models.py
+++ api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Skill(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    type = models.CharField(max_length=40)
+
+
+class Contact(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    value = models.CharField(max_length=40)
+
+
+class UserProject(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    date = models.DateField()
+
+
+class UserAuthentication(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    username = models.CharField(max_length=40)
+    password = models.BigIntegerField()
+    emailAddress = models.CharField(max_length=50)
+    is_company = models.BooleanField()
+
+
+class Company(models.Model):
+    user = models.OneToOneField(
+        UserAuthentication,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    photo = models.ImageField()
+
+
+class User(models.Model):
+    user = models.OneToOneField(
+        UserAuthentication,
+        on_delete=models.CASCADE
+    )
+    frst_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    photo = models.ImageField()
+    sills = models.ForeignKey(
+        Skill,
+        on_delete=models.CASCADE
+    )
+    description = models.TextField()
+    course = models.IntegerField()
+    specialization = models.CharField(max_length=40)
+    university = models.CharField(max_length=40)
+    contacts = models.ForeignKey(
+        Contact,
+        on_delete=models.CASCADE
+    )
+    is_authorised = models.BooleanField()
+    projects = models.ForeignKey(
+        UserProject,
+        on_delete=models.CASCADE
+    )
